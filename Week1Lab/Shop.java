913571e21994ef660f4a6ff2229690277794c59f
Abbyrea2002 <abbyrea2002@gmail.com>
2024-09-25 18:01:08 +0100

    test

--- Week1Lab/Shop.java
+++ Week1Lab/Shop.java
@@ -6,4 +6,41 @@ package Week1Lab;
  */
 public class Shop
 {
+   private int numEmployees;
+   private int averageTurnover;
+
+   public Shop(){
+      /** default constructor calls super class and initalises instance variables*/
+      super();
+      this.numEmployees = 0;
+      this.averageTurnover = 0;
+   }
+   public Shop(String address, String owner,int numEmployees, int averageTurnover){
+      /** overload constructor all variables passed in call super and update house variables */
+      super(address, owner);
+      this.numEmployees = numEmployees;
+      this.averageTurnover = averageTurnover;
+
+   }
+
+   public int getNumEmployees(){
+      return this.numEmployees;
+   }
+
+   public void setNumEmployees(int numEmployees){
+      this.numEmployees = numEmployees;
+   }
+
+   public int getAverageTurnover(){
+      return this.averageTurnover;
+   }
+
+   public void setAverageTurnover(int averageTurnover){
+      this.averageTurnover = averageTurnover;
+   }
+
+   public String toString(){
+
+      return super.toString() + " this shop has " + this.numEmployees + " Employees, with an average turnover of " + this.averageTurnover ;
+   }
 }//class
---
+++ Week1Lab/Test.java
@@ -0,0 +1,19 @@
+package Week1Lab;
+
+/**
+ * Created by abbyr on 25/09/2024
+ * COMMENTS ABOUT PROGRAM HERE
+ */
+public class Test
+{
+   public static void main(String[] args) {
+      Building b1 = new Building();
+      Building b2 = new Building("27 Main Street", "Rex Dog Groomer");
+      Building b3 = new Building("18 Low Street", "Mary Jones");
+      b1.setAddress("3 High Street, Smiths Newsagent");
+      b1.setOwner("Smiths");
+      System.out.println(b1);
+      System.out.println(b2);
+      System.out.println(b3);
+   }
+}//class
